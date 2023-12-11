5cec70b641be8def8e3787f2c7b83da50cda31a5
Raman Bhardwaj <ramanbhardwaj2005@gmail.com>
2023-12-11 22:31:56 +0530

    xd

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import './navbar.css'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 
+
+const lmao = () => {
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt3">What is GPT-3</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Libraries</a></p>
+  </>
+}
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -14,11 +24,7 @@ const Navbar = () => {
         </div>
 
         <div className='gpt3__navbar-links_container'>
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT-3</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Libraries</a></p>
+
         </div>
       </div>
 
